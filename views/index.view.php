13e65a6baad57fcaa4cbb91844674c95b17a6f7b
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-09-05 15:19:47 -0600

    agrege archivos iniciales e imagenes necesarias

---
+++ views/index.view.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/07702d4e25.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Galería</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">My Galería</h1>
+        </div>
+    </header>
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/1.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/2.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/3.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/4.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/5.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/6.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/7.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/8.jpg" alt="primera foto">
+                </a>
+            </div>
+            <div class="paginacion">
+                <a href="#" class="izquierda"><i class="fa fa-arrow-left"></i> Pagina anterior</a>
+                <a href="#" class="derecha">Pagina siguiente<i class="fa fa-arrow-right"></i> </a>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p class="copyright">Galería creada por Enrique Tapia - www.enriquetapiaan.com</p>
+    </footer>
+</body>
+</html>
